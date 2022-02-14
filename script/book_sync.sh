e16b09b63e541d16688bc67f2269ee05ca7aa14b
hikhhbro <w1121508831@gmail.com>
2022-02-14 23:24:12 +0800

    修改同步脚本
    Signed-off-by: hikhhbro <w1121508831@gmail.com>

--- script/book_sync.sh
+++ script/book_sync.sh
@@ -1,1 +1,1 @@
-rsync -avzu -e  'ssh -p 59422  -i /var/services/homes/hik/.ssh/hikvps'   hik-vps@42.193.200.153:/data/calibre/books  /volume2/calibre_bak/books/ -y
+rsync -avzu -e  'ssh -p 59422  -i /var/services/homes/hikhhbro/.ssh/hik-ssh/hikvps'   hik-vps@42.193.200.153:/data/calibre/books/  /volume2/bak/calibre_bak/books/
