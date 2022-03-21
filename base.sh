1873c6e9b96c1f356c35b6c75c239dbcdc118f03
hikhhbro <w1121508831@gmail.com>
2022-03-21 21:39:42 +0800

    添加db 选项

--- base.sh
+++ base.sh
@@ -18,6 +18,7 @@ self_arg=()
 rerun_j=1
 opts="--help --rerun= --build --rootfs --download= --script --code"
 pre_opts="native adb gits"
+db="_task"
 function _func() {
   echo -en "\033[33mCurrent$FUNCNAME => (${FUNCNAME[1]}): \033[0m"
   for i in "$*"      #在"$*"中遍历参数，此时"$*"被扩展为包含所有位置参数的单个字符串，只遍历一次
