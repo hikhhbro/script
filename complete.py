8416e3c1f0120ff772d9821b3ae20cb362db912c
hikhhbro <w1121508831@gmail.com>
2022-04-16 21:57:52 +0800

    提示功能移植成python

--- complete.py
+++ complete.py
@@ -1,22 +1,90 @@
 import os
+import sys
 class Complete:
-    def __init__(self,asgs=''):
+    def __init__(self,asgs=[]):
+        self.end = asgs[-1]
         self.asgs = asgs
-        self.out = 'y '
+        self.asgs.pop()
+        self.input = []
+        self.out = []
+        self.public_com = ['--rm','--code']
+        self.setspace = {True : 'compopt +o nospace',False :'compopt -o nospace'}
+        self.end_dic = {'n': False, 'y': True}
         self.root_dir = os.getenv('HIK_SCRIPT_TOP_DIR')
-    def set_complete(self):
-        self.file_name("script")
-    def set_out(self):
-        print(self.out)
+        
+    def set_out(self,sw:bool):
+        print(self.setspace[sw])
+        print(' '.join(self.out))
+    
+    def isexecutable(self,file):
+        if '.sh' in file or '.' not in file :
+            return True
+        else:
+            return False
+    
     def file_name(self,name=''):
         for root ,dirs,files in os.walk(self.root_dir+'/'+name):
             for d in dirs:
                 if d[0] != '.':
-                    self.out =self.out +d + '/ '  
-            self.out += ' '.join(files)
+                    self.input.append([ 'root','{}/'.format(d)])
+            for file in files:
+                if self.isexecutable(file) :
+                    self.input.append([ 'root','{}'.format(file)])
             break
+        
+    def get_input(self):
+        out = []
+        for item in self.input:
+            out.append(item[-1])
+        return out
+            
+    def matching(self,file,inlist):
+        out = []
+        for it in inlist:
+            l = len(file[-1])
+            if l < len(it) and file[-1] == it[0:l] :
+                if len(file) > 1:
+                    out.append(''.join(file[0:-1])+it)
+                else:
+                    out.append(it)    
+        if not out and (self.end == 'y' or '/' == self.asgs[-1][-1] ):
+            out = self.get_input()
+        return out
+    
+    def compgen(self,file):
+        if  len(self.asgs) == 3 and '-' in file[-1] :
+            self.out = self.matching(file, self.public_com)
+        else:
+            self.out = self.matching(file, self.get_input())
 
+    def getlsspace(self):
+        if len(self.out) == 1 and self.out[0][-1] == '/':
+            return False
+        else:
+            return True
+        
+    def isend(self):
+        return self.end_dic[self.end]
+
+    def find_files_dirs(self,file_dir):
+        for item in file_dir:
+            self.file_name(item)
+
+    def set_complete(self) -> bool :
+        file_dir = ["script",'company'] 
+        file = [self.asgs[-1]]
+        if '/' in self.asgs[-1] and not self.isend() :
+            file =  [self.asgs[-1][:self.asgs[-1].rfind('/')] + '/']
+            for i in range(len(file_dir)):
+                file_dir[i] = file_dir[i] + "/" + file[0]
+            file.append(self.asgs[-1][self.asgs[-1].rfind('/')+1:])
+            if file[-1] == '' :
+                file.pop()
+        self.find_files_dirs(file_dir)
+        self.compgen(file)
+        return self.getlsspace()
+      
 if __name__ == '__main__':
-    com = Complete()
-    com.set_complete()
-    com.set_out()
+    com = Complete(sys.argv)
+    # com = Complete(['hikrun','wiz/w','n'])
+    com.set_out(com.set_complete())
--- hik_run_complete.sh
+++ hik_run_complete.sh
@@ -1,110 +1,110 @@
-__get_file() {
-        # set -x
-         trap exit 0 3
-        COMPREPLY=()
-        unset input2
-        local  input2 
-        # echo "${cur}" "dcur"
-        if [[ "${cur}" == "" ]];then
-          if [ -d "$1" ] && [ -d "$2" ];then 
-            input1="`ls $1`"" `ls $2`"
-            dir1=$1
-            dir2=$2
-          fi
+# __get_file() {
+#         # set -x
+#          trap exit 0 3
+#         COMPREPLY=()
+#         unset input2
+#         local  input2 
+#         # echo "${cur}" "dcur"
+#         if [[ "${cur}" == "" ]];then
+#           if [ -d "$1" ] && [ -d "$2" ];then 
+#             input1="`ls $1`"" `ls $2`"
+#             dir1=$1
+#             dir2=$2
+#           fi
 
-          for i in ${input1}
-          do
-              if [[ "${i}" != *.* ]] ; then
-                input2=${input2}" "$i
-                if [ -d "$1$i" ] || [ -d "$2$i" ];then
-                  input2=${input2}"/"
-                fi
-              fi
-          done
-          COMPREPLY=( $(compgen -W "${input2}" -- ) )
-          # echo "  111r"
-        else
-          if [[ ${cur} != */ ]];then
-            if [[ ${cur} != */* ]];then
-              if [ -d "$1" ] && [ -d "$2" ];then 
-                input1="`ls $1`"" `ls $2`"
-                dir1=$1
-                dir2=$2
-              fi
-              cur_cur=${cur}
-            else
-                temp_cur=${cur%/*}
-                cur_cur=${cur##*/}
-              if [ -d "$1${temp_cur}" ];then
-                  input1="`ls $1${temp_cur}`"
-              elif [ -d "$2${temp_cur}" ];then
-                  input1="`ls $2${temp_cur}`"
-              else
-                echo $1 "32"
-                  echo $1${temp_cur} "32" "  "${cur}
-                return 0
-              fi
-            fi
+#           for i in ${input1}
+#           do
+#               if [[ "${i}" != *.* ]] ; then
+#                 input2=${input2}" "$i
+#                 if [ -d "$1$i" ] || [ -d "$2$i" ];then
+#                   input2=${input2}"/"
+#                 fi
+#               fi
+#           done
+#           COMPREPLY=( $(compgen -W "${input2}" -- ) )
+#           # echo "  111r"
+#         else
+#           if [[ ${cur} != */ ]];then
+#             if [[ ${cur} != */* ]];then
+#               if [ -d "$1" ] && [ -d "$2" ];then 
+#                 input1="`ls $1`"" `ls $2`"
+#                 dir1=$1
+#                 dir2=$2
+#               fi
+#               cur_cur=${cur}
+#             else
+#                 temp_cur=${cur%/*}
+#                 cur_cur=${cur##*/}
+#               if [ -d "$1${temp_cur}" ];then
+#                   input1="`ls $1${temp_cur}`"
+#               elif [ -d "$2${temp_cur}" ];then
+#                   input1="`ls $2${temp_cur}`"
+#               else
+#                 echo $1 "32"
+#                   echo $1${temp_cur} "32" "  "${cur}
+#                 return 0
+#               fi
+#             fi
 
-            for i in ${input1}
-            do
-                if [[ "${i}" != *.* ]] ; then
-                  input2=${input2}" "$i
-                  if [ -d "$1$i" ] || [ -d "$2$i" ];then
-                    input2=${input2}"/"
-                  fi
-                fi
-            done
-            compopt -o nospace
-            COMPREPLY=( $(compgen -W "${input2}" -- ${cur_cur}) )
-            if [[ ${cur} == */* ]];then
-                  compopt +o nospace
-                  for(( i=0;i<${#COMPREPLY[@]};i++)) do
-                    COMPREPLY[$i]="${temp_cur}/${COMPREPLY[$i]}"
-                  done; 
-            else
-                if [[ "${#COMPREPLY[@]}" == "1" && ${COMPREPLY[0]} != */ ]];then
-                    compopt +o nospace
-                fi
-            fi
+#             for i in ${input1}
+#             do
+#                 if [[ "${i}" != *.* ]] ; then
+#                   input2=${input2}" "$i
+#                   if [ -d "$1$i" ] || [ -d "$2$i" ];then
+#                     input2=${input2}"/"
+#                   fi
+#                 fi
+#             done
+#             compopt -o nospace
+#             COMPREPLY=( $(compgen -W "${input2}" -- ${cur_cur}) )
+#             if [[ ${cur} == */* ]];then
+#                   compopt +o nospace
+#                   for(( i=0;i<${#COMPREPLY[@]};i++)) do
+#                     COMPREPLY[$i]="${temp_cur}/${COMPREPLY[$i]}"
+#                   done; 
+#             else
+#                 if [[ "${#COMPREPLY[@]}" == "1" && ${COMPREPLY[0]} != */ ]];then
+#                     compopt +o nospace
+#                 fi
+#             fi
 
 
 
           
           
-          else
-            temp_cur=${cur%/*}
-            if [ -d "$1${temp_cur}" ];then
-                input1="`ls $1${temp_cur}`"
-            elif [ -d "$2${temp_cur}" ];then
-                input1="`ls $2${temp_cur}`"
-            else
-              # echo $1 "dd"
-              return 0
-            fi
-            for i in ${input1}
-            do
-                if [[ "${i}" != *.* ]] ; then
-                  input2=${input2}" "$i
-                  if [ -d "$1$i" ] || [ -d "$2$i" ];then
-                    input2=${input2}"/"
-                  fi
-                fi
-            done
-            compopt -o nospace
-            COMPREPLY=( $(compgen -W "${input2}" -- ) )
-            if [[ "${#COMPREPLY[@]}" == "1"  ]];then
-                    compopt +o nospace
-                    COMPREPLY[0]="${temp_cur}/${COMPREPLY[0]}"
+#           else
+#             temp_cur=${cur%/*}
+#             if [ -d "$1${temp_cur}" ];then
+#                 input1="`ls $1${temp_cur}`"
+#             elif [ -d "$2${temp_cur}" ];then
+#                 input1="`ls $2${temp_cur}`"
+#             else
+#               # echo $1 "dd"
+#               return 0
+#             fi
+#             for i in ${input1}
+#             do
+#                 if [[ "${i}" != *.* ]] ; then
+#                   input2=${input2}" "$i
+#                   if [ -d "$1$i" ] || [ -d "$2$i" ];then
+#                     input2=${input2}"/"
+#                   fi
+#                 fi
+#             done
+#             compopt -o nospace
+#             COMPREPLY=( $(compgen -W "${input2}" -- ) )
+#             if [[ "${#COMPREPLY[@]}" == "1"  ]];then
+#                     compopt +o nospace
+#                     COMPREPLY[0]="${temp_cur}/${COMPREPLY[0]}"
                     
 
-            fi
-          fi
+#             fi
+#           fi
           
-        fi
+#         fi
                   
 
-}
+# }
 
 # _hikrun() {
 #     local pre cur tmp_opt
@@ -214,12 +214,20 @@ __get_file() {
 # }
 _hikrun () {
     COMPREPLY=()
-    pre=${COMP_WORDS[COMP_CWORD-1]}
-    cur=${COMP_WORDS[COMP_CWORD]} 
-    res=$(python3 ${HIK_SCRIPT_TOP_DIR}/complete.py)
-    res=${res:1}
-    COMPREPLY=( $(compgen -W "${res}" -- ${cur}) )   
-    # unset HIK_COMPLETE            
+    local end
+    if [[ "${COMP_LINE:0-1}" == " " ]];then
+      end='y'
+    else
+      end='n'
+    fi
+    
+    local res=( $(python3 ${HIK_SCRIPT_TOP_DIR}/complete.py ${COMP_WORDS[*]} ${end}) )
+    local lsnospace=${res[*]:0:3}
+    unset res[0]
+    unset res[1]
+    unset res[2]
+    ${lsnospace}
+    COMPREPLY=( ${res[*]} )        
 }
 __hikrun_program="hikrun"
 have ${__hikrun_program} && \
