2e7b88ab7c8704632c28beb388e4ae785f58fac7
hikhhbro <w1121508831@gmail.com>
2022-06-01 14:03:22 +0800

    Log: 添加自己的json类 Version: JIRA/Description: Influence:
    Signed-off-by: hikhhbro <w1121508831@gmail.com>

---
+++ hikrun_json.py
@@ -0,0 +1,18 @@
+import json
+import os
+class hikrun_json:
+    def __init__(self, file_path=''):
+        self.file = os.getenv('HIK_SCRIPT_TOP_DIR') + '/' + file_path
+    
+    def str_to_json(self,text) :
+        return text
+    
+    def write_json(self,text):
+        json_data = str_to_json(text)
+        with open(self.file ,"w+") as wf:
+            js = json.dumps(json_data,indent=1)
+            wf.write(js)
+    def read_json(self):    
+        with open(self.file) as rf:
+            json_data = json.load(rf)
+        return json_data
