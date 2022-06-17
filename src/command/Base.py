2a715c1ea6182afb390b6741f911bfcfd8c62931
hikhhbro <w1121508831@gmail.com>
2022-06-17 13:06:04 +0800

    修复adb 回车进shell 出错问题

--- src/command/Base.py
+++ src/command/Base.py
@@ -15,8 +15,9 @@ class Arg():
 
         if self.arg_list[0][-3:] == '.py':
             del self.arg_list[0]
-        if not self.isend:
+        if self.isend == False:
             self.cur =   self.arg_list[-1]
+            del  self.arg_list[-1]
         else :
             self.cur = ''
         
@@ -42,50 +43,51 @@ class Base(Arg):
 class Myclass():
     def __init__(self):
         sys.path.append(os.getenv('SCRIPT_TOP_DIR')+"/src/utils")
-        tmp = copy.copy(sys.argv)
-        self.isend = True if tmp[-1] == 'y' else False
-        if tmp[-1]  in ['n','y']:
-            self.asgs_list = tmp[1:-1] if "complete.py" in tmp[0] or "main.py" in tmp[0] else tmp[:-1]   
-        else:
-            self.asgs_list = tmp
-        if self.isend :
-            self.cur = [''] 
-        else :
-            self.cur = [self.asgs_list[-1]] 
-            del self.asgs_list[-1]  
+        # tmp = copy.copy(sys.argv)
+        # self.isend = True if tmp[-1] == 'y' else False
+        # if tmp[-1]  in ['n','y']:
+        #     self.asgs_list = tmp[1:-1] if "complete.py" in tmp[0] or "main.py" in tmp[0] else tmp[:-1]   
+        # else:
+        #     self.asgs_list = tmp
+        # if self.isend :
+        #     self.cur = [''] 
+        # else :
+        #     self.cur = [self.asgs_list[-1]] 
+        #     del self.asgs_list[-1]  
+        self.arg = Arg()
     def get_sub_tool(self):
-        out  = self.asgs_list[0]
+        out  = self.arg.arg_list[0]
         model = importlib.import_module(out.capitalize())
         return getattr(model,out.capitalize())
     
     def get_class(self):
-        if len(self.asgs_list) == 1 and self.asgs_list[0] == os.getenv('SCRIPT_TOOL_NAME') :
+        if len(self.arg.arg_list) == 1 and self.arg.arg_list[0] == os.getenv('SCRIPT_TOOL_NAME') :
             out = "main"
             package='main'
-        else:
-            out=""
-        del self.asgs_list[0]
-        if self.isend :
-            if len(self.asgs_list) > 0:
-                out = self.asgs_list[0].capitalize()
-                del self.asgs_list[0]
         else :
-            if len(self.asgs_list) > 0:
-                out =  self.asgs_list[0].capitalize()
-                del self.asgs_list[0]
+            out=""
+        del self.arg.arg_list[0]
+        if self.arg.isend :
+            if len(self.arg.arg_list) > 0:
+                out = self.arg.arg_list[0].capitalize()
+                del self.arg.arg_list[0]
+        else :
+            if len(self.arg.arg_list) > 0:
+                out =  self.arg.arg_list[0].capitalize()
+                del self.arg.arg_list[0]
         model = importlib.import_module(out)
         return getattr(model,out)
 
     def get_opname(self):
-        if self.isend :
-            if len(self.asgs_list) > 0:
-                if self.asgs_list[0][0] != '-':
-                    return  self.asgs_list[0]
+        if self.arg.isend :
+            if len(self.arg.arg_list) > 0:
+                if self.arg.arg_list[0][0] != '-':
+                    return  self.arg.arg_list[0]
             else :
                 return ''    
         else :
-            if len(self.asgs_list) > 0:
-                if self.asgs_list[0][0] != '-':
-                    return  self.asgs_list[0]
+            if len(self.arg.arg_list) > 0:
+                if self.arg.arg_list[0][0] != '-':
+                    return  self.arg.arg_list[0]
             else :
                 return ''    
