06ee3b4feb0c8fbd75ec4e6767d649f647121ea1
hikhhbro <w1121508831@gmail.com>
2024-02-07 19:44:35 +0800

    1.添加默认配置文件,修改json为默认配置文件类

--- src/command/Base.py
+++ src/command/Base.py
@@ -6,147 +6,145 @@ from command.Shell import Shell
 from command.Listdirs import CurFile
 import importlib
 import logging
-class Arg():
+from command.Json import Json
+
+class Arg:
     def __init__(self):
         self.arg_list = copy.copy(sys.argv)
-        if self.arg_list[-1] in  ['n','y']:
-            self.isend  = True if self.arg_list[-1] == 'y' else False
-            del  self.arg_list[-1]
-        else :
+        if self.arg_list[-1] in ["n", "y"]:
+            self.isend = True if self.arg_list[-1] == "y" else False
+            del self.arg_list[-1]
+        else:
             self.isend = None
 
-        if self.arg_list[0][-3:] == '.py' or '/' + os.getenv('SCRIPT_TOOL_NAME') in self.arg_list[0]:
+        if (
+            self.arg_list[0][-3:] == ".py"
+            or "/" + os.getenv("SCRIPT_TOOL_NAME") in self.arg_list[0]
+        ):
             del self.arg_list[0]
         if self.isend == False:
-            self.cur =   self.arg_list[-1]
-            del  self.arg_list[-1]
-        else :
-            self.cur = ''
-            
-    def last_cur(self,num):
+            self.cur = self.arg_list[-1]
+            del self.arg_list[-1]
+        else:
+            self.cur = ""
+
+    def last_cur(self, num):
         if len(self.arg_list) < 2:
             return self.cur
         if self.cur and num < len(self.arg_list):
-            return self.arg_list[-(num+1)]
-        return ''
-          
-        
+            return self.arg_list[-(num + 1)]
+        return ""
 
 
 class Base(Arg):
-    def __init__(self,args_list:list = None):
+    def __init__(self, args_list: list = None,config_name="config.json"):
         super().__init__()
-        self.tool_dir = os.getenv('SCRIPT_TOP_DIR')
+        self.tool_dir = os.getenv("SCRIPT_TOP_DIR")
         self.tool_name = os.getenv("SCRIPT_TOOL_NAME")
         self.option_dic = {}
-        self.data_dir = self.tool_dir + '/data/' 
-    
+        self.data_dir = self.tool_dir + "/data/" + Base.__subclasses__()[-1].__name__.lower() + "/"
+        self.config = Json(self.data_dir + config_name)
+        
+        if not os.path.exists(self.config.path):
+            Shell('mkdir -p '+ self.data_dir + "&& echo '{}'> %s" %(self.config.path)).exec_system()
+        
     def _opt(self):
         return Opt(list(self.option_dic.keys()))
+
     def exec(self):
-        self.option_dic[self.arg_list[1]](self.arg_list[2:]) 
-        
+        self.option_dic[self.arg_list[1]](self.arg_list[2:])
 
 
-        
-
-
-
-class Myclass():
+class Myclass:
     def __init__(self):
-        sys.path.append(os.getenv('SCRIPT_TOP_DIR')+"/src/utils")
+        sys.path.append(os.getenv("SCRIPT_TOP_DIR") + "/src/utils")
         self.arg = Arg()
+
     def get_sub_tool(self):
-        out  = self.arg.arg_list[0]
+        out = self.arg.arg_list[0]
         model = importlib.import_module(out.capitalize())
-        return getattr(model,out.capitalize())
-    
+        return getattr(model, out.capitalize())
+
     def get_class(self):
-        if len(self.arg.arg_list) == 1 and self.arg.arg_list[0] == os.getenv('SCRIPT_TOOL_NAME') :
+        if len(self.arg.arg_list) == 1 and self.arg.arg_list[0] == os.getenv(
+            "SCRIPT_TOOL_NAME"
+        ):
             out = "main"
-            package='main'
-        else :
-            out=""
+            package = "main"
+        else:
+            out = ""
         del self.arg.arg_list[0]
-        if self.arg.isend :
+        if self.arg.isend:
             if len(self.arg.arg_list) > 0:
                 out = self.arg.arg_list[0].capitalize()
                 del self.arg.arg_list[0]
-        else :
+        else:
             if len(self.arg.arg_list) > 0:
-                out =  self.arg.arg_list[0].capitalize()
+                out = self.arg.arg_list[0].capitalize()
                 del self.arg.arg_list[0]
         model = importlib.import_module(out)
-        return getattr(model,out)
+        return getattr(model, out)
 
-    def get_opname(self,last=1):
+    def get_opname(self, last=1):
         if len(self.arg.arg_list) > 0 and last <= len(self.arg.arg_list):
-            if self.arg.arg_list[-last][0] != '-':
-                return  self.arg.arg_list[-last]
+            if self.arg.arg_list[-last][0] != "-":
+                return self.arg.arg_list[-last]
             return ""
-        else :
-            return ''    
+        else:
+            return ""
 
 
-class Opt():
-    def __init__(self,opt=None,file_opt = False):
-        self.opt = {
-          "long":["--help"],
-          "short":[],
-          "sub":[]
-        }
+class Opt:
+    def __init__(self, opt=None, file_opt=False):
+        self.opt = {"long": ["--help"], "short": [], "sub": []}
         self.file_opt = file_opt
-        self.retreat = ''
+        self.retreat = ""
         if opt:
-          if isinstance(opt,list):
-            for item in opt:
-              self.opt_type(item).append(item)
-          else :
-              self.opt_type(opt).append(item)
+            if isinstance(opt, list):
+                for item in opt:
+                    self.opt_type(item).append(item)
+            else:
+                self.opt_type(opt).append(item)
 
     def get_long_opt(self):
-      return self.opt["long"]
+        return self.opt["long"]
 
     def get_short_opt(self):
-      return self.opt["short"]
+        return self.opt["short"]
 
     def get_sub_opt(self):
-      return self.opt["sub"]
+        return self.opt["sub"]
 
-    def set_long_opt(self,opt):
-      if isinstance(opt,list):
-        self.opt["long"] = self.opt["long"] + opt
-      else :
-        self.opt["long"].append(opt)
+    def set_long_opt(self, opt):
+        if isinstance(opt, list):
+            self.opt["long"] = self.opt["long"] + opt
+        else:
+            self.opt["long"].append(opt)
 
-    def set_short_opt(self,opt):
-      if isinstance(opt,list):
-        self.opt["short"] = self.opt["short"] + opt
-      else :
-        self.opt["short"].append(opt)
+    def set_short_opt(self, opt):
+        if isinstance(opt, list):
+            self.opt["short"] = self.opt["short"] + opt
+        else:
+            self.opt["short"].append(opt)
 
-    def set_sub_opt(self,opt):
-      if isinstance(opt,list):
-        self.opt["sub"] = self.opt["sub"] + opt
-      else :
-        self.opt["sub"].append(opt)
-        
+    def set_sub_opt(self, opt):
+        if isinstance(opt, list):
+            self.opt["sub"] = self.opt["sub"] + opt
+        else:
+            self.opt["sub"].append(opt)
 
+    def opt_type(self, arg):
+        if "--" == arg[0:2]:
+            opt = self.get_long_opt()
+        elif "-" in arg[0]:
+            opt = self.get_short_opt()
+        else:
+            opt = self.get_sub_opt()
+        return opt
 
-    def opt_type(self,arg):
-      if '--' == arg[0:2] :
-          opt = self.get_long_opt()
-      elif '-' in arg[0] :
-          opt = self.get_short_opt()
-      else :
-          opt = self.get_sub_opt()
-      return opt
-    
-    def shield_opt(self,scope="all"):
-      if self.retreat :
-        if scope == 'all':
-          self.opt_type(self.retreat).clear()
-        else :
-          self.opt_type(self.retreat).remove(self.retreat)
-        
-  
+    def shield_opt(self, scope="all"):
+        if self.retreat:
+            if scope == "all":
+                self.opt_type(self.retreat).clear()
+            else:
+                self.opt_type(self.retreat).remove(self.retreat)
--- src/command/Json.py
+++ src/command/Json.py
@@ -1,19 +1,31 @@
 import json
 import os
 class Json:
-    def __init__(self, file_path=''):
-        self.file = os.getenv('SCRIPT_TOP_DIR') + '/' + file_path
+    def __init__(self, file_path):
+        self.path = file_path
     
     def str_to_json(self,text) :
         return text
     
     def write_json(self,text):
         json_data = self.str_to_json(text)
-        with open(self.file ,"w+") as wf:
+        with open(self.path ,"w+") as wf:
             js = json.dumps(json_data,indent=1)
             wf.write(js)
     def read_json(self):    
-        with open(self.file) as rf:
+        with open(self.path) as rf:
             json_data = json.load(rf)
         return json_data
       
+
+    def write(self,text):
+        json_data = self.str_to_json(text)
+        with open(self.path ,"w+") as wf:
+            js = json.dumps(json_data,indent=1)
+            wf.write(js)
+    def read(self,default=None):    
+        with open(self.path) as rf:
+            json_data = json.load(rf)
+        if default and not json_data:
+          return default
+        return json_data
